fd25604c225050348f5e8b170dab78cb1c3f72e8
bishop254 <karanjachiuri@gmail.com>
2026-05-16 12:03:03 +0300

    Updates

--- constants/api.ts
+++ constants/api.ts
@@ -2,7 +2,8 @@ import Constants from 'expo-constants';
 import { Platform } from 'react-native';
 
 function getDefaultBaseUrl() {
-  const configuredBaseUrl = process.env.EXPO_PUBLIC_API_BASE_URL;
+  // const configuredBaseUrl = process.env.EXPO_PUBLIC_API_BASE_URL;
+  const configuredBaseUrl = 'https://mig-burs.com/zales/';
 
   if (configuredBaseUrl) {
     return configuredBaseUrl;
